4220be6b273c0f9c6dd69cda1b38f6000834cbc7
Eric Thomas <info@powerapps.rocks>
2021-12-15 13:54:50 -0800

    enhancements

--- test.py
+++ test.py
@@ -1,29 +1,41 @@
 import json
-from urllib.parse import urlparse
 import urllib.request
+import urllib.parse
 from bs4 import BeautifulSoup
 
 # ---------------------------------------------------
 
-def create_filename(url):
-    url = urlparse(url).netloc
-    source = url.split('.')[1]
+def get_source(url):
+    url = urllib.parse.urlparse(url).netloc
+    source = url.split('.')
+    if len(source) == 2:
+        source = source[0]
+    elif len(source) == 3:
+        source = source[1]
+    elif len(source) == 4:
+        source = source[1]
     return source
 
 
 def get_rss(url):
-    header= {'User-Agent': 'Mozilla/5.0'}
-    request = urllib.request.Request(url=url, headers=header)
-    response = urllib.request.urlopen(request)
-    bsoup = BeautifulSoup(response, 'xml')
-    return bsoup
+    try:
+        header= {'User-Agent': 'Mozilla/5.0'}
+        request = urllib.request.Request(url=url, headers=header)
+        response = urllib.request.urlopen(request)
+        bsoup = BeautifulSoup(response, 'xml')
+        return bsoup
+    except:
+        print(url)
+        return None
 
 
-def get_headlines(bsoup_obj):
+def get_headlines(bsoup_obj, source):
     headlines = []
-    story = bsoup_obj.find_all('item')
+    story = bsoup_obj.find_all('item') 
     for item in story:
         headline = {
+            "source": source,
+            "pubDate": item.pubDate.text if item.pubDate is not None else 'Unknown',
             "headline": item.title.text,
             "link": item.link.text
         }
@@ -32,32 +44,18 @@ def get_headlines(bsoup_obj):
 
 
 def save_json(data, filename):
-    with open(f'{filename}.json', 'w') as f:
+    with open(f'./output/{filename}.json', 'w') as f:
         json.dump(data, f, indent=2)
         f.close()
 
 
 # ---------------------------------------------------
 
-urls = [
-    'https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml',
-    'https://news.ycombinator.com/rss',
-    'http://rss.cnn.com/rss/cnn_topstories.rss',
-    'https://www.latimes.com/california/rss2.0.xml',
-    'https://www.chicagotribune.com/arcio/rss/category/news/breaking/',
-    'https://timesofindia.indiatimes.com/rssfeedstopstories.cms',
-    'https://www.aljazeera.com/xml/rss/all.xml',
-    'https://feeds.bbci.co.uk/news/video_and_audio/news_front_page/rss.xml',
-    'http://feeds.foxnews.com/foxnews/latest',
-    'https://feeds.washingtonpost.com/rss/local',
-    'http://feeds.marketwatch.com/marketwatch/topstories/',
-    'https://www.theguardian.com/world/rss'
-    # 'https://www.seattletimes.com/seattle-news/feed/'
-]
+urls = open('./input/urls.py', 'r')
 
-
-for u in urls:
-    file_name = create_filename(u)
-    response = get_rss(u)
-    data = get_headlines(response)
-    save_json(data, file_name)
+for u in urls.readlines():
+    if '#' not in u:
+        source = get_source(u)
+        response = get_rss(u)
+        data = get_headlines(response, source)
+        save_json(data, source)
